8689e7a078ceacf870339c0cd53c121ae1f556e8
takaya tanaka <takayahilton@gmail.com>
2015-08-06 17:18:23 +0900

    add test

--- src/test/scala/s99Test.scala
+++ src/test/scala/s99Test.scala
@@ -9,16 +9,22 @@ import NinetyNineScalaProblems.workingList
 
 class s99Test extends Properties("s99") {
 
-  property("length") = forAll{ (xs:List[Int],ys:List[Int]) =>
-    workingList.length(xs) +  workingList.length(ys) == workingList.length(xs ::: ys)
+  property("length") = forAll { (xs: List[Int], ys: List[Int]) =>
+    workingList.length(xs) + workingList.length(ys) == workingList.length(xs ::: ys)
   }
 
-  property("reverse") = forAll{ list:List[Int]=>
+  property("reverse") = forAll { list: List[String] =>
     workingList.reverse(list) == workingList.reverse(workingList.reverse(list))
   }
 
-  property("compress") = forAll{ list:List[String]=>
-    workingList.compress(list) == list.distinct
+  property("compress") = forAll { list: List[Int] =>
+    workingList.compress(list) == workingList.compress(workingList.compress(list))
+  }
+
+  property("range") = forAll(choose(-1000, 1000), choose(-1000, 1000)) { (n: Int, m: Int) =>
+    workingList.range(n, m).length == {
+      if (n <= m) m - n + 1 else 0
+    }
   }
 
 
