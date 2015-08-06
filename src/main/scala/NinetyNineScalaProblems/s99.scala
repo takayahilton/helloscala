36a26193dc9bde2f7054eb457cdfe9b510589b0c
takaya tanaka <takayahilton@gmail.com>
2015-08-06 18:51:32 +0900

    change type List to Seq

--- src/main/scala/NinetyNineScalaProblems/s99.scala
+++ src/main/scala/NinetyNineScalaProblems/s99.scala
@@ -1,88 +1,86 @@
 package NinetyNineScalaProblems
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader
-
 /**
  * Created by tim on 15/08/05.
  */
 object workingList {
 
-  def last[A](list: List[A]): A = list match {
-    case head :: Nil => head
-    case _ :: tail => last(tail)
+  def last[A](seq: Seq[A]): A = seq match {
+    case head +: Seq() => head
+    case _ +: tail => last(tail)
     case _ => throw new NoSuchElementException
   }
 
-  def penultimate[A](list: List[A]): A = list match {
-    case _ :: next :: _ :: Nil => next
-    case head :: tail => penultimate(tail)
+  def penultimate[A](seq: Seq[A]): A = seq match {
+    case _ +: next +: _ +: Seq() => next
+    case head +: tail => penultimate(tail)
     case _ => throw new NoSuchElementException
   }
 
-  def nth[A](n: Int, list: List[A]): A = list match {
-    case Nil => throw new NoSuchElementException
-    case head :: _ if n == 0 => head
-    case _ :: tail => nth(n - 1, tail)
+  def nth[A](n: Int, seq: Seq[A]): A = seq match {
+    case Seq() => throw new NoSuchElementException
+    case head +: _ if n == 0 => head
+    case _ +: tail => nth(n - 1, tail)
   }
 
-  def length[A](list: List[A]): Int = list.foldLeft(0)((total, _) => total + 1)
+  def length[A](seq: Seq[A]): Int = seq.foldLeft(0)((total, _) => total + 1)
 
-  def reverse[A](list: List[A]): List[A] = list.foldLeft(Nil: List[A])((reversList, head) => head :: reversList)
+  def reverse[A](seq: Seq[A]): Seq[A] = seq.foldLeft(Nil :Seq[A])((reversSeq, head) => head +: reversSeq)
 
-  def isPalindrome[A](list: List[A]): Boolean = list == list.reverse
+  def isPalindrome[A](seq: Seq[A]): Boolean = seq == reverse(seq)
 
   //P08
-  def compress[A](list: List[A]): List[A] = list match {
-    case Nil => Nil
-    case head :: tail => head :: compress(tail.dropWhile(_ == head))
+  def compress[A](seq: Seq[A]): Seq[A] = seq match {
+    case Seq() => Seq()
+    case head +: tail => head +: compress(tail.dropWhile(_ == head))
   }
 
   //P09
-  def pack[A](list: List[A]): List[List[A]] = list match {
-    case Nil => Nil
-    case l@(head :: tail) => {
+  def pack[A](seq: Seq[A]): Seq[Seq[A]] = seq match {
+    case Seq() => Seq()
+    case l@(head +: tail) => {
       val t = l.span(_ == head)
-      List(t._1) ::: pack(t._2)
+      Seq(t._1) ++: pack(t._2)
     }
   }
 
   //p10
-  def encode[A](list: List[A]) = pack(list).map(la => la.length -> la.head)
+  def encode[A](seq: Seq[A]) = pack(seq).map(la => la.length -> la.head)
 
   //p11
-  def encodeModified[A](list: List[A]) = encode(list).map {
+  def encodeModified[A](seq: Seq[A]) = encode(seq).map {
     case (1, a) => a
     case x => x
   }
 
   //p12
-  def decode[A](list: List[(Int, A)]): List[A] = list.map {
-    case (i, a) => List.fill(i)(a)
+  def decode[A](seq: Seq[(Int, A)]): Seq[A] = seq.map {
+    case (i, a) => Seq.fill(i)(a)
   }.flatten
 
   //p14
-  def duplicate[A](list: List[A]): List[A] = list.flatMap(a => List(a, a))
+  def duplicate[A](seq: Seq[A]): Seq[A] = seq.flatMap(a => List(a, a))
 
   //p15
-  def duplicateN[A](n: Int, list: List[A]) = list.flatMap(a => List.fill(n)(a))
+  def duplicateN[A](n: Int, seq: Seq[A]) = seq.flatMap(a => List.fill(n)(a))
 
   //p16
-  def drop[A](n: Int, list: List[A]): List[A] = list match {
-    case _ if n < 0 => Nil
-    case Nil => Nil
+  def drop[A](n: Int, seq: Seq[A]): Seq[A] = seq match {
+    case _ if n < 0 => Seq()
+    case Seq() => Seq()
     case l if n == 0 => l
-    case _ :: tail => drop(n - 1, tail)
+    case _ +: tail => drop(n - 1, tail)
   }
 
   //p22
-  def range(n: Int, m: Int): List[Int] = {
-    def go(total: List[Int]): List[Int] = total match {
+  def range(n: Int, m: Int): Seq[Int] = {
+    def go(total: Seq[Int]): Seq[Int] = total match {
       case _ if n > m => total
-      case Nil => go(List(n))
-      case head :: tail if head >= m => total
-      case head :: _ => go((head + 1) :: total)
+      case Seq() => go(Seq(n))
+      case head +: tail if head >= m => total
+      case head +: _ => go((head + 1) +: total)
     }
-    go(Nil).reverse
+    go(Seq()).reverse
   }
 
 }
