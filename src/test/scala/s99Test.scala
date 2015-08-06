4db431fccf21172279edfc1de4a27a5763b83e78
takaya tanaka <takayahilton@gmail.com>
2015-08-06 17:51:36 +0900

    add test

--- src/test/scala/s99Test.scala
+++ src/test/scala/s99Test.scala
@@ -1,4 +1,4 @@
-import org.scalacheck.Properties
+import org.scalacheck.{Gen, Properties}
 import org.scalacheck.Prop.forAll
 import org.scalacheck.Gen.choose
 import NinetyNineScalaProblems.workingList
@@ -19,6 +19,24 @@ class s99Test extends Properties("s99") {
 
   property("compress") = forAll { list: List[Int] =>
     workingList.compress(list) == workingList.compress(workingList.compress(list))
+  }
+
+  property("pack") = forAll { list: List[Int] =>
+    workingList.pack(list).flatten == list
+  }
+
+  property("encode and decode") = forAll { list: List[Int] =>
+    workingList.decode(workingList.encode(list)) == list
+  }
+
+  property("duplicate") = forAll { list: List[Int] =>
+    workingList.duplicate(list).length == list.length * 2
+  }
+
+  val intList = Gen.listOf(Gen.choose(0, 100))
+
+  property("duplicateN") = forAll(choose(0, 100), intList) { (n: Int, list: List[Int]) =>
+    workingList.duplicateN(n, list).length == list.length * n
   }
 
   property("range") = forAll(choose(-1000, 1000), choose(-1000, 1000)) { (n: Int, m: Int) =>
