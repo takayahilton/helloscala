800976a6aea41dc3296471358943ffcb3f15fe76
takaya tanaka <takayahilton@gmail.com>
2015-08-07 13:00:21 +0900

    MyOption追加

---
+++ src/main/scala/MyOption/MyOption.scala
@@ -0,0 +1,62 @@
+package MyOption
+
+import java.util.NoSuchElementException
+
+/**
+ * Created by tim on 15/08/07.
+ */
+
+sealed trait MyOption[+A] {
+
+  def get: A = this match {
+    case MySome(v) => v
+    case MyNone => throw new NoSuchElementException
+  }
+
+  def getOrElse[B >: A](elseValue: B): B = this match {
+    case MySome(v) => v
+    case MyNone => elseValue
+  }
+
+  def isEmpty: Boolean = this match {
+    case MySome(_) => false
+    case MyNone => true
+  }
+
+  def map[B](f: A => B): MyOption[B] = this match {
+    case MySome(a) => MySome(f(a))
+    case MyNone => MyNone
+  }
+
+  def flatMap[B](f: A => MyOption[B]): MyOption[B] = this match {
+    case MySome(a) => f(a)
+    case MyNone => MyNone
+  }
+
+  def filter(f: A => Boolean): MyOption[A] = this match {
+    case MyNone => MyNone
+    case MySome(a) if f(a) => MySome(a)
+    case _ => MyNone
+  }
+
+  def orElse[B >: A](elseValue: => MyOption[B]): MyOption[B] = this match {
+    case MyNone => elseValue
+    case x => x
+  }
+
+
+}
+
+case class MySome[+A](value: A) extends MyOption[A]
+
+case object MyNone extends MyOption[Nothing]
+
+object MyOption {
+  def apply[A](x: A): MyOption[A] = {
+    if (x == null) {
+      MyNone
+    } else {
+      MySome(x)
+    }
+  }
+}
---
+++ src/test/scala/MyOption/MyOptionSpec.scala
@@ -0,0 +1,76 @@
+package MyOption
+
+/**
+ * Created by tim on 15/08/07.
+ */
+
+import org.scalatest.{Matchers, FunSpec}
+
+class MyOptionSpec extends FunSpec with Matchers {
+
+  describe("MyOption#get") {
+    it("should return a value if it's not empty") {
+      MySome(100).get shouldBe 100
+    }
+    it("should throw a NoSuchElementException if it's empty") {
+      intercept[NoSuchElementException] {
+        MyNone.get
+      }
+    }
+  }
+
+  describe("MyOption#isEmpty") {
+    it("should be true if it is empty") {
+      MyNone.isEmpty shouldBe true
+    }
+    it("should be false if it contains a value") {
+      MySome(100).isEmpty shouldBe false
+    }
+  }
+
+  describe("MyOption#map") {
+    it("should return a MySome containing the result of applying f") {
+      MySome(100).map(_ * 2) shouldBe MySome(200)
+    }
+    it("should return MyNone if it is empty") {
+      MyNone.map(_ => false) shouldBe MyNone
+    }
+  }
+
+  describe("MyOption#flatMap") {
+    it("should return the result of applying f") {
+      MySome(100).flatMap(e => MySome(e * 2)) shouldBe MySome(200)
+    }
+    it("should return MyNone if it is empty") {
+      MyNone.flatMap(_ => MySome(false)) shouldBe MyNone
+    }
+  }
+
+  describe("MyOption#filter") {
+    it("should return MySome if the predicate returns true") {
+      MySome(100).filter(_ > 0) shouldBe MySome(100)
+    }
+    it("should return MyNone if the predicate returns false") {
+      MySome(100).filter(_ < 0) shouldBe MyNone
+    }
+  }
+
+  describe("MyOption#getOrElse") {
+    it("should return the MyOption's value if it contains a value") {
+      MySome(100).getOrElse(20) shouldBe 100
+    }
+    it("should return the default value if it is empty") {
+      MyNone.getOrElse(20) shouldBe 20
+    }
+  }
+
+  describe("MyOption#orElse") {
+    it("should return this MyOption value if it is not empty") {
+      MySome(100).orElse(MySome(20)) shouldBe MySome(100)
+    }
+    it("should return the default value if it is empty") {
+      MyNone.orElse(MySome(20)) shouldBe MySome(20)
+    }
+  }
+
+}
