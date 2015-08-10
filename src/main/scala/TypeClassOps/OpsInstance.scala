7a0e94293162680e1e15a6338965d41ac02a31e0
takaya tanaka <takayahilton@gmail.com>
2015-08-10 15:24:47 +0900

    [fix] rename

--- src/main/scala/TypeClassOps/OpsInstance.scala
+++ src/main/scala/TypeClassOps/OpsInstance.scala
@@ -8,18 +8,17 @@ import scala.language.higherKinds
  * Created by tim on 15/08/07.
  */
 
-object OpsInstance{
+object OpsInstance {
 
-  implicit class toMonoidOp[A:Monoid](a1:A) {
-     def |+|(a2:A) = implicitly[Monoid[A]].append(a1,a2)
+  implicit class toMonoidOp[A: Monoid](a1: A) {
+    def |+|(a2: A) = implicitly[Monoid[A]].append(a1, a2)
   }
 
-  implicit class toFunctorOp[F[_]:Functor,A](fa:F[A]){
+  implicit class toFunctorOp[F[_] : Functor, A](fa: F[A]) {
 
-    def map[B](f:A=>B):F[B]= implicitly[Functor[F]].map(fa)(f)
+    def map[B](f: A => B): F[B] = implicitly[Functor[F]].map(fa)(f)
 
   }
-
 
 
 }
