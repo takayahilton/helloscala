9f5a7eb91861b68c5271f7cd9fa2e05b82be9806
takaya tanaka <takayahilton@gmail.com>
2015-08-20 19:13:35 +0900

    commit

--- src/main/scala/TypeLevel/Operations.scala
+++ src/main/scala/TypeLevel/Operations.scala
@@ -1,66 +1,25 @@
 package TypeLevel
 
+import scala.language.higherKinds
+
 /**
  * Created by tim on 15/08/10.
  */
 
-trait Nat
-
-trait Zero extends Nat
-
-trait Succ[N <: Nat] extends Nat
-
-object Nat {
-  type _0 = Zero
-  type _1 = Succ[_0]
-  type _2 = Succ[_1]
+trait Fold[-Elem, Value] {
+  type Apply[E <: Elem, V <: Value] <: Value
 }
 
-
-trait ToInt[N <: Nat] {
-  def apply(): Int
+sealed trait Nat {
+  type Match[NonZero[N <: Nat] <: Up, IfZero <: Up, Up] <: Up
+  type FoldR[Init <: Type, Type, F <: Fold[Nat, Type]] <: Type
 }
 
-object ToInt {
-  def apply[N <: Nat](implicit toInt: ToInt[N]) = toInt()
-
-  implicit def zero = new ToInt[Zero] {
-    def apply() = 0
-  }
-
-  implicit def succ[N <: Nat](implicit toInt: ToInt[N]) = new ToInt[Succ[N]] {
-    def apply() = toInt() + 1
-  }
+sealed trait Zero extends Nat {
+  type Match[NonZero[N <: Nat] <: Up, IfZero <: Up, Up] = IfZero
 }
 
-trait Add[N <: Nat, M <: Nat] {
-  type Result <: Nat
+sealed trait Succ[N <: Nat] extends Nat {
+  type Match[NonZero[N <: Nat] <: Up, IfZero <: Up, Up] = IfZero
 }
-
-object Add {
-  type Type[N <: Nat, M <: Nat, L <: Nat] = N Add M {type Result = L}
-
-  implicit def zero[N<:Nat] = new Add[N,Zero]{
-    type Result = N
-  }
-
-  implicit def addM[N<:Nat , M<:Nat](implicit aux:Aux[N,M,Zero]) = new Add[N,Succ[M]]{
-    type Result = aux.Result
-  }
-
-}
-
-trait Aux[N<:Nat,M<:Nat,L<:Nat]{
-  type Result <: Nat
-}
-
-object Aux{
-  implicit def addM[N<:Nat,M<:Nat,L<:Nat](implicit aux:Aux[N,M,Succ[L]]) = new Aux[Succ[N],Succ[M],L]{
-    type Result = aux.Result
-  }
-}
-
-
-
-
 
